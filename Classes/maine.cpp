1c33bb092bb1cb0de546cadf3b7c3e901c71cb5f
Andre Popovitch <andre@andreisawesome.com>
2015-02-15 23:23:30 -0500

    camera now points at guy

--- Classes/maine.cpp
+++ Classes/maine.cpp
@@ -1,4 +1,5 @@
 #include "maine.h"
+using namespace cocos2d;
 
 using namespace std;
 
@@ -33,14 +34,34 @@ bool KnightWorld::init()
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 	// create a TMX map
-	auto map = TMXTiledMap::create("map.tmx");
-	addChild(map, 0);
+	auto _tileMap = TMXTiledMap::create("map.tmx"); // note to self, consider using "new" here
+	addChild(_tileMap, 0);
+	_background = _tileMap->layerNamed("mainboard");
 
 	// all tiles are aliased by default, let's set them anti-aliased
-	for (const auto& child : map->getChildren())
+	for (const auto& child : _tileMap->getChildren())
 	{
 		static_cast<SpriteBatchNode*>(child)->getTexture()->setAntiAliasTexParameters();
 	}
+
+	auto objectGroup = _tileMap->objectGroupNamed("objects");
+
+	if (objectGroup == NULL){
+		CCLOG("tile map has no 'objects' object layer");
+		return false;
+	}
+	CCLOG("tile map objects detected");
+
+	ValueMap spawnPoint = objectGroup->objectNamed("Team A spawn");
+
+	int x; istringstream( ((spawnPoint.at("x")).getDescription()) ) >> x;
+	int y; istringstream( ((spawnPoint.at("y")).getDescription()) ) >> y;
+
+	auto _player = Sprite::create("imgs/sprite1.png");
+	_player->setPosition(ccp(x, y));
+
+	this->addChild(_player);
+	this->setViewPointCenter(_player->getPosition());
 
 
 
@@ -49,21 +70,21 @@ bool KnightWorld::init()
 
 
 
-void KnightWorld::menuCloseCallback(Ref* pSender)
-{
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.", "Alert");
-	return;
-#endif
+void KnightWorld::setViewPointCenter(CCPoint position) {
 
-	Director::getInstance()->end();
+	auto winSize = Director::getInstance()->getWinSize();
+	/*auto mapsize = _tileMap->getMapSize();
+	int tilewidth = 32;
+	int tileheight = 32;
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-	exit(0);
-#endif
+	int x = MAX(position.x, winSize.width / 2);
+	int y = MAX(position.y, winSize.height / 2);
+	x = MIN(x, (mapsize.width * tilewidth) - winSize.width / 2);
+	y = MIN(y, (mapsize.height * tileheight) - winSize.height / 2);
+	CCPoint actualPosition = ccp(x, y);
+
+	CCPoint centerOfView = ccp(winSize.width / 2, winSize.height / 2);
+	CCPoint viewPoint = ccpSub(centerOfView, actualPosition);
+	this->setPosition(viewPoint);*/
+	this->setPosition(ccpSub(ccp(winSize.width / 2, winSize.height / 2), position));
 }
-
-
-
-
-
--- Classes/maine.h
+++ Classes/maine.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include "cocos2d.h"
+using namespace cocos2d;
 
 class KnightWorld : public cocos2d::Layer
 {
+private:
+	CCTMXTiledMap *_tileMap;
+	CCTMXLayer *_background;
+	CCSprite *_player;
 public:
 	// there's no 'id' in cpp, so we recommend returning the class instance pointer
 	static cocos2d::Scene* createScene();
@@ -15,7 +20,7 @@ public:
 	void menuCloseCallback(cocos2d::Ref* pSender);
 
 
-
+	void setViewPointCenter(CCPoint position);
 
 
 	// implement the "static create()" method manually
